02f5a04c1cec194009a5a21b8ff9025df8f55900
BrankoKrstic <brankokrstic94@gmail.com>
2021-05-30 17:14:26 +0200

    generate graph when app starts

--- src/components/Pathfinder/Pathfinder.js
+++ src/components/Pathfinder/Pathfinder.js
@@ -1,7 +1,26 @@
+import { useState, useEffect } from "react";
 import { WeightedGraph } from "../../algos/dijkstra";
 import "./Pathfinder.css";
 
+const NUM_ROWS = 20;
+const NUM_COLS = 50;
+
 export default function Pathfinder() {
+	const [gridState, setGridState] = useState({ graph: null });
+	useEffect(() => {
+		const newGraph = new WeightedGraph();
+		for (let i = 0; i < NUM_ROWS * NUM_COLS; i++) {
+			newGraph.addVertex(i);
+			if (i % NUM_COLS !== 0) {
+				newGraph.addEdge(i, i - 1, 1);
+			}
+			if (i > NUM_COLS) {
+				newGraph.addEdge(i, i - NUM_COLS, 1);
+			}
+		}
+		setGridState({ graph: newGraph });
+		console.log(gridState);
+	}, []);
 	return (
 		<div className="Pathfinder">
 			<header className="Pathfinder-nav">Nav goes here</header>
