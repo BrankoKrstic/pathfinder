d4f097444491bb628b98544d7af0ef74d6d588e7
BrankoKrstic <brankokrstic94@gmail.com>
2021-06-06 01:57:49 +0200

    separate priority queue to a separate file

--- src/algos/dijkstra.js
+++ src/algos/dijkstra.js
@@ -1,8 +1,5 @@
 import Stack from "../structures/Stack";
-
-const swap = (arr, idx1, idx2) => {
-	[arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
-};
+import PriorityQueue from "../structures/PriorityQueue";
 
 const calcXDist = (node1, node2, numCols) => {
 	return Math.abs((Number(node1) % numCols) - (Number(node2) % numCols));
@@ -14,75 +11,6 @@ const calcYDist = (node1, node2, numCols) => {
 			Math.floor(Number(node2) / numCols)
 	);
 };
-
-class Node {
-	constructor(val, priority) {
-		this.val = val;
-		this.priority = priority;
-	}
-}
-
-class PriorityQueue {
-	constructor() {
-		this.values = [];
-	}
-	enqueue(val, priority) {
-		let newNode = new Node(val, priority);
-		this.values.push(newNode);
-		this.bubbleUp();
-	}
-	bubbleUp() {
-		let idx = this.values.length - 1;
-		while (idx > 0) {
-			let parentIdx = Math.floor((idx - 1) / 2);
-			if (this.values[idx].priority > this.values[parentIdx].priority)
-				break;
-			swap(this.values, idx, parentIdx);
-			idx = parentIdx;
-		}
-	}
-	dequeue() {
-		const min = this.values[0];
-		const end = this.values.pop();
-		if (this.values.length > 0) {
-			this.values[0] = end;
-			this.sinkDown();
-		}
-		return min;
-	}
-	sinkDown() {
-		let idx = 0;
-		const length = this.values.length;
-		const element = this.values[0];
-		while (true) {
-			let leftChildIdx = 2 * idx + 1;
-			let rightChildIdx = 2 * idx + 2;
-			let leftChild, rightChild;
-			let swapIdx = null;
-
-			if (leftChildIdx < length) {
-				leftChild = this.values[leftChildIdx];
-				if (leftChild.priority < element.priority) {
-					swapIdx = leftChildIdx;
-				}
-			}
-			if (rightChildIdx < length) {
-				rightChild = this.values[rightChildIdx];
-				if (
-					(swapIdx === null &&
-						rightChild.priority < element.priority) ||
-					(swapIdx !== null &&
-						rightChild.priority < leftChild.priority)
-				) {
-					swapIdx = rightChildIdx;
-				}
-			}
-			if (swapIdx === null) break;
-			swap(this.values, idx, swapIdx);
-			idx = swapIdx;
-		}
-	}
-}
 
 export class WeightedGraph {
 	constructor() {
