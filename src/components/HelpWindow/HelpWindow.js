74d68f8d0674ba3adf10536a1ab3175d450630f6
BrankoKrstic <brankokrstic94@gmail.com>
2021-06-19 00:33:44 +0200

    set up help page routing

--- src/components/HelpWindow/HelpWindow.js
+++ src/components/HelpWindow/HelpWindow.js
@@ -1,41 +1,31 @@
-import "./HelpWindow.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import HelpWindowStart from "./HelpWindowStart/HelpWindowStart";
+import HelpWindowAlgos from "./HelpWindowAlgos/HelpWindowAlgos";
 import Modal from "../UI/Modal/Modal";
-import StartButton from "../NavComponents/StartButton/StartButton";
+import "./HelpWindow.css";
 
 export default function HelpWindow(props) {
 	const { toggleHelp } = props;
 	return (
-		<>
+		<Router>
 			<Modal clicked={toggleHelp}>
 				<div className="HelpWindow">
-					<div className="HelpWindowStart">
-						<h1>Welcome to Pathfinder!</h1>
-						<p>
-							Pathfinder is a software that visualizes the search
-							patterns of common algorithms for finding a path in
-							a graph.
-						</p>
-						<p>
-							Some of the algorithms guarantee finding the
-							shortest path while others illustrate different
-							traversal patterns.
-						</p>
-						<p>
-							Feel free to run any algorithm, click and drag to
-							draw obstacles, or play with any of the other
-							options.
-						</p>
-						<p>Most importantly, have fun!</p>
-						<div className="HelpButtons">
-							<StartButton
-								isDark={false}
-								clicked={toggleHelp}
-								text="Ready!"
-							/>
-						</div>
+					<div className="HelpWindowInner">
+						<Switch>
+							<Route exact path="/">
+								<HelpWindowStart toggleHelp={toggleHelp} />
+							</Route>
+							<Route exact path="/legend">
+								<HelpWindowStart />
+							</Route>
+							<Route exact path="/algos">
+								<HelpWindowAlgos />
+							</Route>
+						</Switch>
 					</div>
 				</div>
 			</Modal>
-		</>
+		</Router>
 	);
 }
---
+++ src/components/HelpWindow/HelpWindowStart/HelpWindowStart.js
@@ -0,0 +1,30 @@
+import StartButton from "../../NavComponents/StartButton/StartButton";
+import "./HelpWindowStart.css";
+
+export default function HelpWindowStart(props) {
+	return (
+		<>
+			<h1>Welcome to Pathfinder!</h1>
+			<p>
+				Pathfinder is a software that visualizes the search patterns of
+				common algorithms for finding a path in a graph.
+			</p>
+			<p>
+				Some of the algorithms guarantee finding the shortest path while
+				others illustrate different traversal patterns.
+			</p>
+			<p>
+				Feel free to run any algorithm, click and drag to draw
+				obstacles, or play with any of the other options.
+			</p>
+			<p>Most importantly, have fun!</p>
+			<div className="HelpButtons">
+				<StartButton
+					isDark={false}
+					clicked={props.toggleHelp}
+					text="Ready!"
+				/>
+			</div>
+		</>
+	);
+}
