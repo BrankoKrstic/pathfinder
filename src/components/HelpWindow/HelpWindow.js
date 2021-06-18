f85008fe194bc7e172176800a30ea08a0eef3097
BrankoKrstic <brankokrstic94@gmail.com>
2021-06-18 21:07:50 +0200

    add content to help popup

--- src/components/HelpWindow/HelpWindow.js
+++ src/components/HelpWindow/HelpWindow.js
@@ -6,7 +6,27 @@ export default function HelpWindow(props) {
 	return (
 		<>
 			<Modal clicked={toggleHelp}>
-				<div className="HelpWindow"></div>
+				<div className="HelpWindow">
+					<div className="HelpWindowStart">
+						<h1>Welcome to Pathfinder!</h1>
+						<p>
+							Pathfinder is a software that visualizes the search
+							patterns of common algorithms for finding a path in
+							a graph.
+						</p>
+						<p>
+							Some of the algorithms guarantee finding the
+							shortest path while others illustrate different
+							traversal patterns.
+						</p>
+						<p>
+							Feel free to run any algorithm, click and drag to
+							draw obstacles, or play with any of the other
+							options.
+						</p>
+						<p>Most importantly, have fun!</p>
+					</div>
+				</div>
 			</Modal>
 		</>
 	);
