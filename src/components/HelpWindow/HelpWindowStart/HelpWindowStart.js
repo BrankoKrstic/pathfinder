d776f8057add923233da1fef789f4d456df070a6
BrankoKrstic <brankokrstic94@gmail.com>
2021-06-20 23:01:23 +0200

    style edits

--- src/components/HelpWindow/HelpWindowStart/HelpWindowStart.js
+++ src/components/HelpWindow/HelpWindowStart/HelpWindowStart.js
@@ -5,20 +5,22 @@ import "./HelpWindowStart.css";
 export default function HelpWindowStart(props) {
 	return (
 		<>
-			<h1>Welcome to Pathfinder!</h1>
-			<p>
-				Pathfinder is a software that visualizes search patterns of
-				common algorithms for finding a path in a graph.
-			</p>
-			<p>
-				Some of the algorithms guarantee finding the shortest path while
-				others illustrate different traversal patterns.
-			</p>
-			<p>
-				Feel free to run any algorithm, click and drag to draw
-				obstacles, or play with any of the other options.
-			</p>
-			<p>Most importantly, have fun!</p>
+			<div className="HelpWindowStart-body">
+				<h1>Welcome to Pathfinder!</h1>
+				<p>
+					Pathfinder is a software that visualizes search patterns of
+					common algorithms for finding a path in a graph.
+				</p>
+				<p>
+					Some of the algorithms guarantee finding the shortest path
+					while others illustrate different traversal patterns.
+				</p>
+				<p>
+					Feel free to run any algorithm, click and drag to draw
+					obstacles, or play with any of the other options.
+				</p>
+				<p>Most importantly, have fun!</p>
+			</div>
 			<div className="HelpButtons">
 				<div className="HelpButtons-left">
 					<Link to="/legend">Legend</Link>
